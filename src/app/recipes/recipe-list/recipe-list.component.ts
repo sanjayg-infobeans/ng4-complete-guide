27e2ca86728ad45ac3d020c6ae5517ff7b3d1307
sanjay.gajera@infobeans.com <sanjay.gajera@infobeans.com>
2017-10-03 17:38:56 +0530

    changes

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply test', 'https://natashaskitchen.com/wp-content/uploads/2017/06/Chicken-Fritters-3.jpg'),
+    new Recipe('A Test Recipe', 'This is a simply test', 'https://natashaskitchen.com/wp-content/uploads/2017/06/Chicken-Fritters-3.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
